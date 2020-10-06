241e9639fc544aaf1ca58892cafb9d08fec9c49b
adima511 <50150006+adima511@users.noreply.github.com>
2020-10-06 19:50:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 var port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
  
-    PythonShell.run('bot.py');
+    PythonShell.run('bot.py', function () {
+        console.log('Running python file');
+    });
     // spawn new child process to call the python script
     res.send("Bot activated");
 })
