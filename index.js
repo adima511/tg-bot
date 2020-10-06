e3bdc41a1b866e5b3743c2143fe02db0f2ebea59
adima511 <50150006+adima511@users.noreply.github.com>
2020-10-06 19:54:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-var PythonShell = require('python-shell');
+let {PythonShell} = require('python-shell')
 const app = express()
 var port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
  
-    PythonShell.run('bot.py', function () {
+    PythonShell.run('bot.py', function (err) {
+        if (err) throw err;
         console.log('Running python file');
     });
     // spawn new child process to call the python script
