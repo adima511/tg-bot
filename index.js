c60819139f490e5e3b87147713e95d159e5b112f
adima511 <50150006+adima511@users.noreply.github.com>
2020-10-06 18:59:46 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const {spawn} = require('child_process');
+const app = express()
+const port = 5000
+app.get('/', (req, res) => {
+ 
+    let dataToSend;
+    // spawn new child process to call the python script
+    const python = spawn('python', ['bot.py']);
+    res.send("Bot activated");
+})
+
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
